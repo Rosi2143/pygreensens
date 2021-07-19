576fad7b9a00175608766fa0737b28bd7326e033
Michael <Schrott.Micha@web.de>
2021-07-19 21:28:01 +0200

    Add classes for data structures
    * Notifications
    * Sensors
    * Hubs
    
    First version of a classed based data storage
    
    Signed-off-by: Michael <Schrott.Micha@web.de>

--- src/pygreensens/api.py
+++ src/pygreensens/api.py
@@ -3,11 +3,88 @@
 
 import requests
 import urllib3
+import datetime
+import os
 
 urllib3.disable_warnings()
 import json
 from datetime import date
 
+verbose = False
+
+class GreensensSensor:
+    def __init__(self, json_struct: dict):
+        if verbose : print (json_struct)
+        self._sensorID    = json_struct["sensorID"]
+        print (f"create sensor: {self._sensorID}")
+        self._id          = int(json_struct["id"])
+        self._isReset     = bool(json_struct["isReset"])
+        self._plantId     = int(json_struct["plantId"])
+        self._link        = json_struct["link"]
+        self._plantNameEN = str(json_struct["plantNameEN"])
+        self._plantNameDE = str(json_struct["plantNameDE"])
+        self._plantNameLA = str(json_struct["plantNameLA"])
+        self._date        = datetime.datetime.fromtimestamp(json_struct["lastConnection"])
+        if not self._isReset: self._chargeLevel = int(json_struct["chargeLevel"])
+        self._data        = json_struct
+
+    def isSensorActive(self):
+        if verbose: print (f"reset = {self._isReset}")
+        return not self._isReset
+
+    def return_data(self, onlyActive=False):
+        return self._data
+
+    def getSensorID(self, onlyActive=False):
+        return self._sensorID
+
+
+class GreensensNotification:
+    def __init__(self, json_struct: dict):
+        if verbose : print (json_struct)
+        self._date        = datetime.datetime.fromtimestamp(json_struct["date"])
+        self._message     = str(json_struct["message"])
+        print (f"create notification: {self._message}")
+        if json_struct["plantModel"] != None:
+            self._sensorID    = json_struct["plantModel"]["sensorID"]
+            self._plantId     = json_struct["plantModel"]["plantId"]
+        else:
+            self._sensorID    = 0
+            self._plantId     = 0
+
+    def print(self):
+        return str(self._date) + "::" + self._message
+
+class GreensensHub:
+    def __init__(self, name: str):
+        print (f"create Hub: {name}")
+        self._name = name
+        self._sensorList = []
+
+    def addSensor(self, sensor: GreensensSensor):
+        self._sensorList.append(sensor)
+
+    def return_num_of_sensors(self, onlyActive=False):
+        numOfSensors = 0
+        for sensor in self._sensorList:
+            if sensor.isSensorActive() or not onlyActive:
+                numOfSensors += 1
+        return numOfSensors
+
+    def return_data(self, onlyActive=False):
+        data = {}
+        for sensor in self._sensorList:
+            if sensor.isSensorActive() or not onlyActive:
+                data.update(sensor.return_data())
+        return data
+
+    def return_sensors(self, onlyActive=False):
+        """Return sensor data"""
+        data = []
+        for sensor in self._sensorList:
+            if sensor.isSensorActive() or not onlyActive:
+                data.append(sensor.getSensorID())
+        return data
 
 class GreensensApi:
     def __init__(self, username: str, password: str):
@@ -22,14 +99,14 @@ class GreensensApi:
 
         self._bearer = f"Bearer {self._at}"
         self._headers = {"Content-Type": "application/json"}
-        self._data = None
-        # self._hubs = None
-        self._sensors = None
-        self._num_of_hubs = 0
-        self._num_of_sensors = 0
+
+        self._hubList = []
+        self._notificationList = []
+
+        self._notifications = None
+        self._num_of_notifications = 0
 
         self.update()
-        self.update_sensors()
 
     def return_is_authenticated(self):
         return (self._at != None)
@@ -37,24 +114,30 @@ class GreensensApi:
     def return_last_error(self):
         return self._error
 
-    def return_data(self):
+    def return_data(self, onlyActive=False):
         """Return sensor data"""
+        data = {}
         self.update()
-        return self._data
+        for hub in self._hubList:
+            data.update(hub.return_data(onlyActive))
 
-    def return_sensors(self):
+        return data
+
+    def return_sensors(self, onlyActive=False):
         """Return sensor data"""
-        # self.update()
-        self.update_sensors()
-        return self._sensors
+        data = []
+        for hub in self._hubList:
+            data += hub.return_sensors(onlyActive)
+        return data
 
-    def update_sensors(self):
-        """Return sensor list"""
-        list = []
-        if self._data != None:
-            for key, value in self._data.items():
-                list.append(key)
-        self._sensors = list
+    def return_notifications(self):
+        """Return notifications data"""
+        data = ""
+        self.get_notification()
+        for notification in self._notificationList:
+            data += notification.print() + os.linesep
+
+        return data
 
     def update(self):
         """Update sensor data"""
@@ -74,17 +157,41 @@ class GreensensApi:
             if data.status_code == 200:
                 hubs = data.json()["data"]["registeredHubs"]
                 new_data = {}
-                self._num_of_hubs = len(hubs)
-                self._num_of_sensors = 0
+                self._hubList.clear()
                 for hub in hubs:
-                    self._num_of_sensors += len(hub["plants"])
+                    hub_obj = GreensensHub(hub["name"])
                     for sensor in hub["plants"]:
+                        sensor_obj = GreensensSensor(sensor)
+                        hub_obj.addSensor(sensor_obj)
                         new_data[sensor["sensorID"]] = sensor
+                    self._hubList.append(hub_obj)
                 self._error = "OK"
                 return new_data
             else:
                 self._error = f"HTTP error: " + str(data.status_code) + " for " + data.request.url
                 return self._data
+
+    ## HTTP REQUEST ##
+    def get_notification(self):
+        if self._at != None:
+            """Make a request."""
+            url = f"{self._host}/users/notifications"
+            self.update_access_token()
+            headers = self._headers
+            headers["authorization"] = self._bearer
+            data = self.s.get(
+                url, headers=headers, verify=False, timeout=10
+            )
+            if data.status_code == 200:
+                notifications = data.json()["data"]["notifications"]
+                self._num_of_notifications = len(notifications)
+                for notification in notifications:
+                    self._notificationList.append(GreensensNotification(notification))
+                self._error = "OK"
+                return
+            else:
+                self._error = f"HTTP error: " + str(data.status_code) + " for " + data.request.url
+                return
 
     ## AUTH ##
     def authenticate(self):
@@ -109,9 +216,15 @@ class GreensensApi:
             self.authenticate()
 
     def return_num_of_hubs(self):
-        return self._num_of_hubs
+        return len(self._hubList)
 
-    def return_num_of_sensors(self):
-        return self._num_of_sensors
+    def return_num_of_sensors(self, onlyActive=False):
+        numOfSensors = 0
+        for hub in self._hubList:
+            numOfSensors += hub.return_num_of_sensors(onlyActive)
+        return numOfSensors
+
+    def return_num_of_notifications(self):
+        return self._num_of_notifications
 
 ##===============================##
